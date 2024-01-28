b1c15bdcd19837af35469828ff1704d621a701db
Scott Ho <sscott.ho@gmail.com>
2024-01-28 16:18:39 -0700

    Update Dialog 1-1.gd

--- Scripts/Dialog Scripts/Dialog 1-1.gd
+++ Scripts/Dialog Scripts/Dialog 1-1.gd
@@ -2,7 +2,6 @@ extends Area2D
 const DialogBox = preload("res://Scripts/Dialog Box.gd")
 var dialog_manager_scene = preload("res://Scenes/dialog_manager.tscn")
 var dialog_manager_instance = null
-@onready var transition_sprite = $"../../../transition_sprite"
 
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D" and dialog_manager_instance == null and !GlobalVars.turn_battle_complete:
